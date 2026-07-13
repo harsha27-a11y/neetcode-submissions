11881f87a1c74661dd594aae78bd40de5bf2c1e4
harsha27-a11y <harshavardhanmmurugesan@gmail.com>
2026-07-13 11:48:05 +0530

    Add: number-of-senior-citizens - submission-3

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-3.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c=0;
+        for(int i=0;i<details.length;i++){
+            if((details[i].charAt(11)=='6' && details[i].charAt(12) != '0' ) || details[i].charAt(11) > '6') c++;
+        }
+        return c;
+    }
+}
